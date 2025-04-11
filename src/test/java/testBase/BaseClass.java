4ed666bca1c18fe7f54bc49f3501f3cf5828d6b2
anubhavrajput193@gmail.com <anubhavrajput193@gmail.com>
2025-04-12 02:45:23 +0530

    reports deleted

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,19 +1,14 @@
 package testBase;
 
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Date;
 import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.MutableCapabilities;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -95,19 +90,6 @@ public class BaseClass {
 
 	public void navigateToAlertsPage() {
 		driver.get(alertUrl);
-	}
-
-	public String captureScreen(String tname) {
-		String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-
-		TakesScreenshot takeScreenshot = (TakesScreenshot) driver;
-		File sourceFile = takeScreenshot.getScreenshotAs(OutputType.FILE);
-
-		String targetFilePath = System.getProperty("user.dir") + "\\screenshots\\" + tname + "_" + timestamp;
-		File targetFile = new File(targetFilePath);
-
-		sourceFile.renameTo(targetFile);
-		return targetFilePath;
 	}
 
 }
--- src/test/java/utilities/ExtentReportsUtility.java
+++ src/test/java/utilities/ExtentReportsUtility.java
@@ -28,14 +28,11 @@ public class ExtentReportsUtility implements ITestListener {
 
 	public void onStart(ITestContext testContext) {
 		String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-
 		repName = "Test-Report-" + timestamp + ".html";
 		sparkReporter = new ExtentSparkReporter(".\\reports\\" + repName);
-
 		sparkReporter.config().setDocumentTitle("Automation Report");
 		sparkReporter.config().setReportName("Funtional Testing");
 		sparkReporter.config().setTheme(Theme.DARK);
-
 		extent = new ExtentReports();
 		extent.attachReporter(sparkReporter);
 		extent.setSystemInfo("Application", "HeroKuapp");
@@ -54,13 +51,6 @@ public class ExtentReportsUtility implements ITestListener {
 		test = extent.createTest(result.getTestClass().getName());
 		test.log(Status.FAIL, result.getName() + "got failed");
 		test.log(Status.INFO, result.getThrowable().getMessage());
-
-		try {
-			String imgPath = new BaseClass().captureScreen(result.getName());
-			test.addScreenCaptureFromPath(imgPath);
-		} catch (Exception e1) {
-			e1.getMessage();
-		}
 	}
 
 	public void onTestSkipped(ITestResult result) {
@@ -78,7 +68,6 @@ public class ExtentReportsUtility implements ITestListener {
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-
 
 	}
 }
